0363327f91d4e9b5de053f461d7fa1c4b2c8f235
amemiya <74266094+amemiya@users.noreply.github.com>
2025-06-11 02:57:53 +0900

    f

--- build/scripts/build-mbedtls.sh
+++ build/scripts/build-mbedtls.sh
@@ -5,6 +5,7 @@ SRC_DIR=$(pwd)/mbedtls
 BUILD_DIR=$(pwd)/build/mbedtls-ios
 
 git clone --recursive https://github.com/Mbed-TLS/mbedtls.git "$SRC_DIR" || true
+source ./venv/bin/activate
 cmake -S "$SRC_DIR" -B "$BUILD_DIR" \
   -DCMAKE_TOOLCHAIN_FILE="$(pwd)/ios-cmake/ios.toolchain.cmake" \
   -DPLATFORM=OS64 \
