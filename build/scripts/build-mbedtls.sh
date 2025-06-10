324d75015cb09e2fa020999a0eedadd49626007d
amemiya <74266094+amemiya@users.noreply.github.com>
2025-06-11 02:32:45 +0900

    f

--- build/scripts/build-mbedtls.sh
+++ build/scripts/build-mbedtls.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SRC_DIR=$(pwd)/mbedtls
 BUILD_DIR=$(pwd)/build/mbedtls-ios
 
-git clone https://github.com/ARMmbed/mbedtls.git "$SRC_DIR" || true
+git clone --recursive https://github.com/Mbed-TLS/mbedtls.git "$SRC_DIR" || true
 cmake -S "$SRC_DIR" -B "$BUILD_DIR" \
   -DCMAKE_TOOLCHAIN_FILE="$(pwd)/ios-cmake/ios.toolchain.cmake" \
   -DPLATFORM=OS64 \
