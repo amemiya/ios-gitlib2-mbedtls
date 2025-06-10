4c022448d1602ea3fcef37f623b663419eba49fa
amemiya <74266094+amemiya@users.noreply.github.com>
2025-06-11 03:28:14 +0900

    f

--- build/scripts/build-libssh2.sh
+++ build/scripts/build-libssh2.sh
@@ -11,7 +11,7 @@ export OPENSSL_LIBRARIES=""
 export OPENSSL_INCLUDE_DIR=""
 
 git clone https://github.com/libssh2/libssh2.git "$SRC_DIR" || true
-cmake -S "$SRC_DIR" -B "$BUILD_DIR" \
+cmake --trace-expand -S "$SRC_DIR" -B "$BUILD_DIR" \
   -DCMAKE_TOOLCHAIN_FILE="$(pwd)/ios-cmake/ios.toolchain.cmake" \
   -DPLATFORM=OS64 \
   -DENABLE_ZLIB_COMPRESSION=ON \
@@ -22,6 +22,8 @@ cmake -S "$SRC_DIR" -B "$BUILD_DIR" \
   -DMBEDX509_LIBRARY="$MB_LIB/libmbedx509.a" \
   -DMBEDCRYPTO_LIBRARY="$MB_LIB/libmbedcrypto.a" \
   -DLIBSSH2_USE_SHARED_LIBS=OFF \
+  -DLIBSSH2_USE_OPENSSL=OFF \
+  -DLIBSSH2_BUILD_EXAMPLES=OFF \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=OFF
 
