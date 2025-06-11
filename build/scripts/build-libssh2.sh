4840f5ff5d570a6e9ad1156d0c61ea8d8badad61
amemiya <74266094+amemiya@users.noreply.github.com>
2025-06-11 09:45:05 +0900

    f

--- build/scripts/build-libssh2.sh
+++ build/scripts/build-libssh2.sh
@@ -15,18 +15,18 @@ cmake -S "$SRC_DIR" -B "$BUILD_DIR" \
   -DCMAKE_TOOLCHAIN_FILE="$(pwd)/ios-cmake/ios.toolchain.cmake" \
   -DPLATFORM=OS64 \
   -DENABLE_ZLIB_COMPRESSION=ON \
-  -DLIBSSH2_MBEDTLS=ON \
-  -DLIBSSH2_CRYPTO_BACKEND=mbedtls \
+  -DMBEDTLS=ON \
+  -DCRYPTO_BACKEND=mbedtls \
   -DMBEDTLS_INCLUDE_DIRS="$MB_INC" \
   -DMBEDTLS_LIBRARY="$MB_LIB/libmbedtls.a" \
   -DMBEDX509_LIBRARY="$MB_LIB/libmbedx509.a" \
   -DMBEDCRYPTO_LIBRARY="$MB_LIB/libmbedcrypto.a" \
-  -DLIBSSH2_USE_SHARED_LIBS=OFF \
-  -DLIBSSH2_USE_OPENSSL=OFF \
-  -DLIBSSH2_BUILD_EXAMPLES=OFF \
+  -DUSE_SHARED_LIBS=OFF \
+  -DUSE_OPENSSL=OFF \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=OFF \
-  -DLIBSSH2_BUILD_TESTS=OFF \
-  -CRYPTO_BACKEND=mbedtls
+  -DBUILD_TESTS=OFF \
+  -CRYPTO_BACKEND=mbedtls \
+  -DBUILD_EXAMPLES=OFF \
 
-cmake --build "$BUILD_DIR" --config Release
+cmake --build "$BUILD_DIR" --target install >/dev/null 2>/dev/null
